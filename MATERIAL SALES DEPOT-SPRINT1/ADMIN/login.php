9a232292ae05a72c352c20f1eb421f891fcd766e
aniitta67 <74583366+aniitta67@users.noreply.github.com>
2022-01-23 23:26:24 -0900

    Update login.php

--- MATERIAL SALES DEPOT-SPRINT1/ADMIN/login.php
+++ MATERIAL SALES DEPOT-SPRINT1/ADMIN/login.php
@@ -11,7 +11,7 @@ if($_POST)
     $query="SELECT * FROM `login` WHERE `username`='$username' AND `password`='$password'";
     $result=$con->query($query);
 
-    // echo mysqli_num_rows($result);exit;
+   
 
     if(mysqli_num_rows($result)>0)
     {
@@ -114,6 +114,6 @@ if($_POST)
    </body>
 </html>
 
-<!-- PHP BEGINS HERE -->
+
 
 
