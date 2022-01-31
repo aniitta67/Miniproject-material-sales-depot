5b4514c1a90b3e734470673adb000d3f9019726c
aniitta67 <74583366+aniitta67@users.noreply.github.com>
2022-01-31 11:35:19 +0530

    Add files via upload

---
+++ sprint2/userhome.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Material sales depot</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../Index/assets/img/hero-bg.jpg" rel="icon">
+  <link href="../Index/assets/img/hero-bg.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
+
+
+
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Abel|Satisfy' rel='stylesheet' type='text/css' />
+<link href="../Index/assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="../Index/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="../Index/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="../Index/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="../Index/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="../Index/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="../Index/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<link href="../Index/assets/css/style1.css" rel="stylesheet">
+<link href="../Index/assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <div id="wrapper">
+	<div id="header-wrapper">
+		<div id="header" class="container">
+  <!-- <header id="header" class="fixed-top ">-->
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="../Index/index.html">material depot<span>.</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      
+
+      <a href="../Index/index.html" class="get-started-btn scrollto">HOME</a>
+
+    </div>
+</div>
+</div>
+</div>
+
+  </header><!-- End Header -->
+	<!-- end #header -->
+
+
+
+  <main id="main">
+
+
+
+	<!-- ======= Material Section ======= -->
+    <section id="team" class="team">
+		<div class="container" data-aos="fade-up">
+  
+		  <div class="section-title">
+			<h2>MATERIALS</h2>
+			<p>Building construction materials</p>
+		  </div>
+  
+		  <div class="row">
+  
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+			  <div class="member" data-aos="fade-up" data-aos-delay="100">
+				<div class="member-img">
+				  <img src="../Index/assets/img/team/1.jpeg" class="img-fluid" alt="">
+				  <div class="social">
+					
+					<a href="materialdetails/malabarcement.php" class="get-started-btn scrollto">BUY</a>
+					
+				  </div>
+				</div>
+				<div class="member-info">
+				  <h4>MALABAR CLASSIC CEMENT</h4>
+				  <span>Rs 330/Bag</span>
+				</div>
+			  </div>
+			</div>
+  
+			<div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/2.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/mahacement.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>MAHA  CEMENT</h4>
+				  <span>Rs 330/Bag</span>
+				  </div>
+				</div>
+			  </div>
+  
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/3.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/zuaricement.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>ZUARI CEMENT</h4>
+					<span>Rs 410/Bag</span>
+				  </div>
+				</div>
+			  </div>
+  
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/4.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/dalmiacement.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>DALMIA CEMENT</h4>
+					<span>Rs 390/Bag</span>
+				  </div>
+				</div>
+			  </div>
+
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/5.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/acccement.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>ACC CEMENT</h4>
+					<span>Rs 420/Bag</span>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/6.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/kenza.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+				  <h4>Kenza TMT Steel</h4>
+					<span>Rs 70/kg</span>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/7.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/agni.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+				  <h4>Agni TMT Steel</h4>
+					
+					<span>Rs 70/kg</span>
+				  </div>
+				</div>
+			  </div>
+  
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/13.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/concreteblock.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>Concrete Block</h4>
+					<span>Rs 70/piece</span>
+				  </div>
+				</div>
+			  </div>
+ 
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/14.jpeg" class="img-fluid" alt="">
+					<div class="social">
+						<br> <br> <br>
+					  
+					  <a href="materialdetails/aacblock.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					  <br>  <br>  <br>   <br>  <br>  <br>
+					<h4>Rectangle Grey AAC Block</h4>
+					<span>Rs 70/piece</span>
+				  </div>
+				</div>
+			  </div>
+  
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/15.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="materialdetails/clayhollowblock.php" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>Rectangular Porotherm Clay Hollow Blocks </h4>
+					<span>Rs 70/piece </span>
+				  </div>
+				</div>
+			  </div>
+
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/16.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="#" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>Walter White</h4>
+					<span>Chief Executive Officer</span>
+				  </div>
+				</div>
+			  </div>
+			  <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+				<div class="member" data-aos="fade-up" data-aos-delay="100">
+				  <div class="member-img">
+					<img src="../Index/assets/img/team/17.jpeg" class="img-fluid" alt="">
+					<div class="social">
+					  
+					  <a href="#" class="get-started-btn scrollto">BUY</a>
+					  
+					</div>
+				  </div>
+				  <div class="member-info">
+					<h4>Walter White</h4>
+					<span>Chief Executive Officer</span>
+				  </div>
+				</div>
+			  </div>
+			  
+  
+		  </div>
+  
+		</div>
+	  </section><!-- End Team Section -->
+  
+	  
+
+    
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>MATERIALS</h2>
+          <p>Related Items</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+             <div class="icon-box"> 
+
+			<!--	<div class="member-img">-->
+				<img src="../Index/assets/img/team/8.jpeg" class="img-fluid" alt="">
+           <!--   <div class="icon"><i class="bx bxl-dribbble"></i></div> -->
+              <h4><a href="">Lorem Ipsum</a></h4>
+              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+         <!--     <div class="icon"><i class="bx bx-file"></i></div> -->
+		 <img src="../Index/assets/img/team/10.jpeg" class="img-fluid" alt="">
+
+              <h4><a href="">Sed ut perspiciatis</a></h4>
+              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+				<img src="../Index/assets/img/team/11.jpeg" class="img-fluid" alt="">
+              <h4><a href="">Magni Dolores</a></h4>
+              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+				<img src="../Index/assets/img/team/12.jpeg" class="img-fluid" alt="">
+              <h4><a href="">Nemo Enim</a></h4>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+				<img src="../Index/assets/img/team/18.jpeg" class="img-fluid" alt="">
+              <h4><a href="">Dele cardo</a></h4>
+              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+            </div>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+   
+
+    
+
+   
+            
+
+
+    
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>MATERIAL DEPOT<span>.</span></h3>
+            
+         <a href="#"></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../Index/assets/vendor/purecounter/purecounter.js"></script>
+  <script src="../Index/assets/vendor/aos/aos.js"></script>
+  <script src="../Index/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../Index/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="../Index/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../Index/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="../Index/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../Index/assets/js/main.js"></script>
+
+</body>
+
+</html>
